0eb6c614711654956c9b494f5efb64ab237b27ec
Nathan Stitt <nathan@stitt.org>
2024-01-19 11:47:11 -0500

    add sample config file

---
+++ include/upnext_config_sample.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#define CALENDAR_DEPLOYMENT_ID "SET_ME"
+
+// set your key here
+#define OPEN_WEATHER_API_KEY "SET_ME"
+
+// how soon should the upcoming event be focused
+#define UPCOMING_MINS 30
+
+// location to request weather from
+#define LATITUDE "27.590890"
+// "38.6018799"
+#define LONGITUDE "-82.546979"
+// -92.1503735"
+
+#define WIFI_SSID "SET_ME"
+#define WIFI_PASS "SET_ME"
+#define DEFAULT_TIME_SERVER "pool.ntp.org"
+
+// chicago https://github.com/nayarsystems/posix_tz_db/blob/master/zones.csv
+// Destrioit (eastern) "EST5EDT,M3.2.0,M11.1.0"
+#define TIMEZONE "CST6CDT,M3.2.0,M11.1.0"
+//
+
+#define CONFIG_WIFI_MAXIMUM_RETRY 10
+
+#define SCREEN_WIDTH 480
+#define SCREEN_HEIGHT 320
